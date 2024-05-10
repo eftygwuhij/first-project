97248ac52b4915c3c853a6471771cdd5ca3d8d7e
eftygwuhij <nsjcicolesebben@hotmail.com>
2024-05-10 09:02:13 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const number = 10;
-if (number % 2 === 0) {
-  console.log("Even number");
-} else {
-  console.log("Odd number");
+function combinationSum(candidates, target) {
+  const result = [];
+  backtrack(0, [], 0);
+  return result;
+  function backtrack(start, current, sum) {
+    if (sum === target) {
+      result.push([...current]);
+      return;
+    }
+    if (sum > target || start === candidates.length) return;
+    for (let i = start; i < candidates.length; i++) {
+      current.push(candidates[i]);
+      backtrack(i, current, sum + candidates[i]);
+      current.pop();
+    }
+  }
 }
