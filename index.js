669f98233d547c536a79dbe8e790d47bb514b370
eftygwuhij <nsjcicolesebben@hotmail.com>
2024-05-12 13:21:46 +0700

    start project

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-function combinationSum4(nums, target) {
-  const dp = new Array(target + 1).fill(0);
-  dp[0] = 1;
-  for (let i = 1; i <= target; i++) {
-    for (const num of nums) {
-      if (i >= num) {
-        dp[i] += dp[i - num];
-      }
-    }
-  }
-  return dp[target];
-}
+const average = (arr) => arr.reduce((a, v) => a + v, 0) / arr.length;
