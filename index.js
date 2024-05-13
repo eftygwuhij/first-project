e0a6928916faf35dd8ca10c6e348cdefeaf55bf3
eftygwuhij <nsjcicolesebben@hotmail.com>
2024-05-14 03:20:26 +0700

    init project

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const average = (arr) => arr.reduce((a, v) => a + v, 0) / arr.length;
+function levelOrder(root) {
+  if (!root) return [];
+  const result = [];
+  const queue = [root];
+  while (queue.length) {
+    const levelSize = queue.length;
+    const currentLevel = [];
+    for (let i = 0; i < levelSize; i++) {
+      const node = queue.shift();
+      currentLevel.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    result.push(currentLevel);
+  }
+  return result;
+}
