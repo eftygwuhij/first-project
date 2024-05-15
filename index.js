f47cb79725e358f2e0305d63098a5502026187a3
eftygwuhij <nsjcicolesebben@hotmail.com>
2024-05-15 16:39:17 +0700

    hello world commit

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-function reverseBetween(head, m, n) {
-  if (!head || m === n) return head;
-  const dummy = new ListNode(0);
-  dummy.next = head;
-  let prev = dummy;
-  for (let i = 0; i < m - 1; i++) prev = prev.next;
-  let start = prev.next;
-  let then = start.next;
-  for (let i = 0; i < n - m; i++) {
-    start.next = then.next;
-    then.next = prev.next;
-    prev.next = then;
-    then = start.next;
+const binarySearchRecursive = (
+  arr,
+  target,
+  left = 0,
+  right = arr.length - 1,
+) => {
+  if (left > right) {
+    return -1;
   }
-  return dummy.next;
-}
+  const mid = Math.floor((left + right) / 2);
+  if (arr[mid] === target) {
+    return mid;
+  } else if (arr[mid] < target) {
+    return binarySearchRecursive(arr, target, mid + 1, right);
+  } else {
+    return binarySearchRecursive(arr, target, left, mid - 1);
+  }
+};
